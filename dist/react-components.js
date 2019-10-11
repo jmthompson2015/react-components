3aaf86a9a9e48290e35ea3421b233d3f44b74d3b
jmthompson2015 <softwaredev@jeffreythompson.net>
2019-10-11 10:45:08 -0600

    Changed to center the content of the TitledElement.

--- dist/react-components.js
+++ dist/react-components.js
@@ -316,10 +316,10 @@
 
   class TitledElement extends React.Component {
     render() {
-      const { className, element, title, titleClass } = this.props;
+      const { className, element, elementClass, title, titleClass } = this.props;
 
       const titleCell = ReactUtilities.createCell(title, "titleCell", titleClass);
-      const elementCell = ReactUtilities.createCell(element, "elementCell");
+      const elementCell = ReactUtilities.createCell(element, "elementCell", elementClass);
 
       const rows = [ReactUtilities.createRow(titleCell, "titleRow"), ReactUtilities.createRow(elementCell, "elementRow")];
 
@@ -332,11 +332,13 @@
     title: PropTypes.string.isRequired,
 
     className: PropTypes.string,
+    elementClass: PropTypes.string,
     titleClass: PropTypes.string
   };
 
   TitledElement.defaultProps = {
     className: "bg-light-gray ma1",
+    elementClass: "ma0 tc v-mid",
     titleClass: "b f5 ph1 pt1 tc"
   };
 
--- view/TitledElement.js
+++ view/TitledElement.js
@@ -2,10 +2,10 @@ import RU from "./ReactUtilities.js";
 
 class TitledElement extends React.Component {
   render() {
-    const { className, element, title, titleClass } = this.props;
+    const { className, element, elementClass, title, titleClass } = this.props;
 
     const titleCell = RU.createCell(title, "titleCell", titleClass);
-    const elementCell = RU.createCell(element, "elementCell");
+    const elementCell = RU.createCell(element, "elementCell", elementClass);
 
     const rows = [RU.createRow(titleCell, "titleRow"), RU.createRow(elementCell, "elementRow")];
 
@@ -18,11 +18,13 @@ TitledElement.propTypes = {
   title: PropTypes.string.isRequired,
 
   className: PropTypes.string,
+  elementClass: PropTypes.string,
   titleClass: PropTypes.string
 };
 
 TitledElement.defaultProps = {
   className: "bg-light-gray ma1",
+  elementClass: "ma0 tc v-mid",
   titleClass: "b f5 ph1 pt1 tc"
 };
 
